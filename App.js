dcb80276d7a99d2d440ce713dd0839d0a0e612b4
Himal Jayawardane <Himal@Admins-MacBook-Pro.local>
2018-08-20 18:40:44 +0530

    Some text chaged in app.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+       <Text> Some text</Text>
         <TextInput />
       </View>
     );
