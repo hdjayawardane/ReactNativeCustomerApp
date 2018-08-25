d89be2b3fa3d098ca0e6e1d2ef1f4be413354122
Himal Jayawardane <Himal@Admins-MacBook-Pro.local>
2018-08-25 13:31:43 +0530

    Text changed ti Gara

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-       <Text> Some text</Text>
+       <Text> Gara</Text>
         <TextInput />
       </View>
     );
